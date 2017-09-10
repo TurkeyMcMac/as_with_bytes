26d842ecae1f6d485db5e741721ae293cbf3de4a
TurkeyMcMac <jwmhjwmh@gmail.com>
2017-09-10 07:58:03 -0400

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,65 @@
 #![no_std]
 
+//! Using these traits, values can be serialized as bytes without any copying of data whatsoever.
+//! 
+//! The generalization that is used here only works for data which contains no pointers to other
+//! data. As such, the traits are only implemented for types which implement `Copy` and for slices
+//! whose contents implement `Copy`.
+//! 
+//! This crate makes no guarantees about portability across systems; it simply encodes the raw
+//! bytes of values.
+//! 
+//! #### It's all the same block of memory
+//! ```rust
+//! use as_with_bytes::{AsBytes, WithBytes};
+//! 
+//! use std::ptr;
+//! 
+//! let arr = [10, -11];
+//! 
+//! // They reference the same memory address
+//! assert!(ptr::eq(unsafe { <[i32; 2]>::with_bytes(arr.as_bytes()) }, &arr));
+//! ```
+
 use core::mem;
 use core::slice;
 
+/// A trait used for converting into bytes.
 pub trait AsBytes {
+    /// Returns a byte slice representation of `self`.
     fn as_bytes<'a>(&'a self) -> &'a [u8];
 }
 
+/// A trait used for converting from bytes.
 pub trait WithBytes {
+    /// Returns a `Self` representation of the given slice of bytes.
+    ///
+    /// # Unsafe
+    /// This function is unsafe for two reasons: Firstly, If the
+    /// length of `bytes` is shorter than `size_of::<Self>`,
+    /// arbitrary memory is read. Secondly, invalid values can
+    /// be returned, such as an instance of an empty enum.
+    /// This method will work fine as long as you are careful to
+    /// avoid both scenarios.
     unsafe fn with_bytes<'a>(bytes: &'a [u8]) -> &'a Self;
 }
 
+
+/// A trait for converting from bytes while checking that the byte
+/// slice is long enough.
 pub trait TryWithBytes {
+    /// Returns `Some(&Self)` if there are enough bytes to encode `Self`,
+    /// or `None` otherwise.
+    /// 
+    /// # Unsafe
+    /// While this protects against reading from memory beyond the boundary
+    /// of the bytes, it can still produce invalid data for some types such
+    /// as enums. It will work as long as whatever you encode from a type,
+    /// you decode into that same type.
+    /// 
+    /// #### Note
+    /// When used to decode dynamically sized slices, `Some` will always be
+    /// returned, since the slice will be empty if there is not enough data.
     unsafe fn try_with_bytes<'a>(bytes: &'a [u8]) -> Option<&'a Self>;
 }
 
